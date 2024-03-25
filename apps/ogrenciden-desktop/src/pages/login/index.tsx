20864a012d47707a59886b9e468817d42b4c57a7
dogruvolkan <volkandogru11@gmail.com>
2024-03-25 15:30:47 +0300

    fix: bu bir deneme

--- apps/ogrenciden-desktop/src/pages/login/index.tsx
+++ apps/ogrenciden-desktop/src/pages/login/index.tsx
@@ -12,6 +12,7 @@ import 'react-toastify/dist/ReactToastify.css';
 export const Login = () => {
 
     console.log("test")
+    console.log("test")
 
     const [login , setLogin] = useState<Auth.Login>({
         UserName: "",
