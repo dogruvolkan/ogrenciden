e7421e91ae74978193eed99ba7838711c46b501a
dogruvolkan <volkandogru11@gmail.com>
2024-03-29 15:30:38 +0300

    docs:hello

--- apps/ogrenciden-desktop/src/pages/notices/create/index.tsx
+++ apps/ogrenciden-desktop/src/pages/notices/create/index.tsx
@@ -17,6 +17,7 @@ interface Props {
 export const CreateNoticesContainer = (props:Props) =>{
     console.log("selam");
     console.log("selam2")
+    console.log("selam2")
 
     const {categories ,cities,universities} = props;
 
